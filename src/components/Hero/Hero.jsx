bdc110e661177b92aff86f147f5ee195c216ce49
Sean Tarzy <seantarzy@gmail.com>
2021-02-17 17:19:51 -0500

    page loads without template

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -23,6 +23,7 @@ const Header = () => {
 
   return (
     <section id="hero" className="jumbotron">
+          {name ? 
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
@@ -42,6 +43,7 @@ const Header = () => {
           </p>
         </Fade>
       </Container>
+          : null}
     </section>
   );
 };
